0b548db5f7be26c0023d83fbcbfe247ff8819dba
Jason Beetham <beefers331@gmail.com>
2024-04-28 22:12:46 -0600

    Started on adding entities

--- data/spaceentity.nim
+++ data/spaceentity.nim
@@ -14,8 +14,7 @@ type
 
   EntityKind = enum
     Asteroid
-    Planet
-    Star
+    Projectile
     Ship
     Station
 
@@ -28,15 +27,78 @@ type
     Jiborn
     #Custom = 16
 
-  SpaceEntity = object
+  SystemKind = enum
+    Sensor
+    WeaponBay # Turrets, Missiles bays, ...
+    ToolBay # Drills, welders, ...
+    Thruster
+    Shield
+    Nanites
+    AutoLoader
+    AutoTargetting
+    Hacker
+    Room
+
+  WeaponKind = enum
+    Bullet
+    Guass
+    RocketVolley
+    Missile
+    Nuke
+
+  ToolKind = enum
+    Drill
+    Welder
+
+  SystemFlag = enum
+    Powered
+    Jammed
+
+  System = object
+    name: string
+    powerUsage: int
+    flags: set[SystemFlag]
+    case kind: SystemKind
+    of Sensor:
+      sensorRange: int
+    of WeaponBay:
+      weaponTarget: string
+      weaponkind: WeaponKind
+      maxAmmo: int
+      currentAmmo: int
+    of ToolBay:
+      toolTarget: string # Use node id instead?
+    of Shield, Nanites:
+      currentShield, maxShield: int
+      regenRate: int
+    of Thruster:
+      acceleration: float32
+      maxSpeed: float32
+    of AutoLoader:
+      discard
+    of AutoTargetting:
+      autoTarget: string
+    of Hacker:
+      hackSpeed: int
+      hackRange: int
+    of Room:
+      inventory: Compartment
+
+  SpaceEntity* = object
+    node: int # For the tree
     name*: string
     faction*: Faction
-    node: int
     x*, y*: float32
     velocity*: float32
+    maxSpeed*: float32
     heading: float32
-    kind: EntityKind
-    compartments: seq[Compartment]
+    case kind: EntityKind
+    of Ship, Station:
+      systems: seq[System]
+    of Asteroid:
+      resources: seq[InventoryEntry]
+    of Projectile:
+      projKind: WeaponKind
 
   Chunk = object
     entities: QuadTree[SpaceEntity]
@@ -50,6 +112,11 @@ type
     randState: Rand
     activeChunks: seq[Chunk] # Load N number of chunks around player
 
+proc canHack*(spaceEntity: SpaceEntity): bool =
+  if spaceEntity.kind in {Ship, Station}:
+    for sys in spaceEntity.systems:
+      if sys.kind == Hacker and Powered in sys.flags:
+        return true
 
 
 proc isReady*(world: World): bool = world.playerName.len != 0
@@ -65,26 +132,39 @@ proc init*(world: var World, playerName, seed: string) =
 
   world.randState = initRand(world.seed)
   var qt = QuadTree[SpaceEntity].init(1000, 1000)
-  world.player = qt.add SpaceEntity(name: playerName, x: 500, y: 500)
+  world.player = qt.add:
+    SpaceEntity(
+      kind: Ship,
+      name: playerName,
+      x: 500,
+      y: 500,
+      maxSpeed: 3,
+      systems: @[
+        System(kind: Sensor, sensorRange: 100),
+        System(kind: Hacker, hackSpeed: 1, hackRange: 100)
+      ]
+    )
   world.activeChunks.add Chunk(entities: qt, nameToEntityInd: {playerName: world.player}.toTable())
 
   const entityNames = ["Freighter", "Asteroid", "Carrier", "Hauler", "Unknown"]
 
-  for _ in 0..100000:
+  for _ in 0..1000:
     let
       selectedName = world.randState.sample(entityNames)
       name = selectedName & $world.activeChunks[0].nameCount.getOrDefault(selectedName)
       x = world.randState.rand(100d..900d)
       y =  world.randState.rand(100d..900d)
-      vel =  world.randState.rand(1d..10d)
+      vel =  world.randState.rand(1d..5d)
       faction = world.randState.rand(Faction)
       heading = world.randState.rand(0d..Tau)
 
     world.activeChunks[0].nameToEntityInd[name] = world.activeChunks[0].entities.add SpaceEntity(
+      kind: if selectedName == entityNames[1]: Asteroid else: Ship,
       name: name,
       x: x,
       y: y,
       velocity: vel,
+      maxSpeed: rand(vel .. 5d),
       faction: faction,
       heading: heading
       )
